c0b59c56921e9e3bf8e5c040631953e4c10ee684
LameCoder17 <gshalom17@gmail.com>
2022-03-26 22:57:38 +0530

    Fixed Static issue

--- lib/Services/Database.dart
+++ lib/Services/Database.dart
@@ -3,20 +3,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shopping_list/Services/Auth.dart';
 
 class Database {
-  static final _db = FirebaseFirestore.instance.collection(AuthenticationHelper().id);
-
   static Future<bool> addItem(Map<String, dynamic> item) async {
-    await _db.add(item);
+    await FirebaseFirestore.instance.collection(AuthenticationHelper().id).add(item);
     return true;
   }
 
   static Future<bool> updateItem(String id, Map<String, dynamic> item) async {
-    await _db.doc(id).update(item);
+    await FirebaseFirestore.instance.collection(AuthenticationHelper().id).doc(id).update(item);
     return true;
   }
 
   static Future<bool> deleteItem(String id) async {
-    await _db.doc(id).delete();
+    await FirebaseFirestore.instance.collection(AuthenticationHelper().id).doc(id).delete();
     return true;
   }
 }
